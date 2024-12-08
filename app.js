457010618bf7b2aba4502d9f2d67adb9d7ef2ab2
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-12-08 20:10:14 +0100

    Pridavam reseni

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+// Oživte pexesové kartičky. Kliknutí na libovolnou kartičku by ji mělo obrátit. Další kliknutí by ji mělo vrátit do původního stavu.
+// 1. Vycházejte ze šablony na GitHubu cviceni-pexeso.
+// 2. Upravte soubor app.js tak, aby pomocí metody querySelectorAll prošel všechny prvky se třídou karticka a přidal jim posluchač události na kliknutí.
+// 3. Pokud událost nastane odeberte nebo přidejte kartičce třídu otocena. Styly už máte nachystané. Kartička s třídou otocena uživateli obrázek skrývá. Po odebrání této třídy se obrázek ukáže.
+
+
 console.log('Funguju!')
+
 
 const karticky = document.querySelectorAll('.karticka')
 
-karticky[0].classList.remove('otocena')
+karticky.forEach(karticka => {   
+     karticka.addEventListener('click', e => {           e.target.classList.toggle('otocena')    })})
